41cee6fa689e2baf2a91d57290a0450ba97f15d5
JayaShreeCloudTechnology <jayashreecloudtechnology@gmail.com>
2026-09-16 22:38:10 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,27 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.ecs.id
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.app.name
+}
+
+output "ecs_task_definition" {
+  value = aws_ecs_task_definition.app.family
+}
